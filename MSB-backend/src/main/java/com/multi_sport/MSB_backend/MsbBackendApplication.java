94c0b2a490e86c8903a6d4154a625241e3b05de2
mohamedelfarouk <mohameda.elfarouk@gmail.com>
2024-07-20 20:40:10 +0300

    first commit

---
+++ MSB-backend/src/main/java/com/multi_sport/MSB_backend/MsbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.multi_sport.MSB_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbBackendApplication.class, args);
+	}
+
+}
---
+++ MSB-backend/src/test/java/com/multi_sport/MSB_backend/MsbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi_sport.MSB_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
