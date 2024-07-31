8866e02093186a530492262df1112bb7f3e31439
mohamedelfarouk <mohameda.elfarouk@gmail.com>
2024-07-31 14:51:13 +0300

    Semi-complete Backend

--- MSB-frontend/src/app/app.routes.ts
+++ MSB-frontend/src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { AthleteComponent } from './athlete/athlete.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  { path: 'sign-up', component: SignUpComponent },
-  {path:'athlete',component:AthleteComponent}
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registeration', component: SignUpComponent },
+  { path: 'athlete', component: AthleteComponent },
 ];
