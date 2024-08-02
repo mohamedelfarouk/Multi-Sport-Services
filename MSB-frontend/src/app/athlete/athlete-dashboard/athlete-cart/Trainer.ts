42cbc79c8755c621f959a2e5c8c8f1b5e0f6ba72
habiba <habibanassersaieed@gmail.com>
2024-08-02 06:55:46 +0300

    almost finished athlete frontend

---
+++ MSB-frontend/src/app/athlete/athlete-dashboard/athlete-cart/Trainer.ts
@@ -0,0 +1,10 @@
+export class Trainer{
+    name:string
+    date:string
+    price:number
+    constructor(name:string,date:string,price:number){
+        this.name=name;
+        this.date=date;
+        this.price =price;
+    }
+    }
